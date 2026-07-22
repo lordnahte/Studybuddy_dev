78c7b72ca7f881b103e537a806c31fbf8d5459de
BigDawgCarbEatingEthanS(i eat dogs) <119267818+lordnahte@users.noreply.github.com>
2026-07-22 13:22:33 +0000

    fd

--- docs/app.js
+++ docs/app.js
@@ -1,32 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import {
-    getAuth,
-    signInWithEmailAndPassword,
-    createUserWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDrYItXUqdvCs181pOjcKM2rojKxNlV458",
-    authDomain: "studybuddy-545e6.firebaseapp.com",
-    projectId: "studybuddy-545e6",
-    storageBucket: "studybuddy-545e6.firebasestorage.app",
-    messagingSenderId: "608044097764",
-    appId: "1:608044097764:web:e5524be095c940fb590cf1",
-    measurementId: "G-HKLJ6R523P"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-window.login = function() {
-    const email = document.getElementById("email").value;
-    const pass = document.getElementById("password").value;
-
-    signInWithEmailAndPassword(auth, email, pass)
-        .then(() => {
-            window.location.href = "Study.html";
-        })
-        .catch(err => alert(err.message));
 import {
     signInWithEmailAndPassword,
     createUserWithEmailAndPassword
@@ -84,7 +55,7 @@ form.addEventListener("submit", (event) => {
             if (mode === "signup") {
                 setMessage("Account created. Taking you in…", "info");
             }
-            window.location.href = "Study.html";
+            window.location.href = "/study";
         })
         .catch((err) => {
             setLoading(false);
@@ -108,4 +79,3 @@ function friendlyError(code) {
             return "Something went wrong. Please try again.";
     }
 }
-
